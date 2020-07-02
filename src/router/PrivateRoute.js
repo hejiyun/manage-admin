79b1dfee2f95c2fcb636082011a9a42ee53fabbb
hejiyun <2879722585@qq.com>
2020-07-02 16:33:43 +0800

    检查无权限路由跳转

--- src/router/PrivateRoute.js
+++ src/router/PrivateRoute.js
@@ -4,7 +4,7 @@ import { Route, Redirect, } from 'react-router-dom'
 // 这里后期可以获取redux中的数据.判断权限,如果没有权限,则会重定向到登录页
 const isAuthenticated = () => {
   // 在这个逻辑函数中, 去进行判断, 是否是后端返回的权限路由
-    return false
+    return true
 }
 // 创建受保护的路由,只有登录状态能访问.
 const PrivateRoute = ({component: Component, ...rest}) => (
